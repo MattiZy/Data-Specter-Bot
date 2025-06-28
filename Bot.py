5c73bdea9a1dcc2e797df1a5efd755c3c36f9494
MattiZy <146745486+MattiZy@users.noreply.github.com>
2025-06-28 22:46:11 +0200

    fix with test command

--- Bot.py
+++ Bot.py
@@ -1,1 +1,28 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+bot = commands.bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+client.run('your token here')
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Data-Specter-Bot
 Only a Fun Project im gonna prbly nver finish
+pip Discord needed
